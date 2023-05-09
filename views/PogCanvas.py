da49025e90696ff0382f0beb40cd63b2815f0132
LLambert <lambertlb@aol.com>
2023-05-09 10:57:22 -0500

    got pog canvas drag working

--- views/PogCanvas.py
+++ views/PogCanvas.py
@@ -1,23 +1,27 @@
 """
 GPL 3 file header
 """
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter
 
 from services.AsyncTasks import AsyncImage
 from services.ServicesManager import ServicesManager
 
 
-class PogCanvas(QtWidgets.QLabel):
+class PogCanvas(QtWidgets.QPushButton):
 	fromRibbonBar = False
 	pogData = None
 	badURL = False
-	showImage = False
 	currentPictureUrl = None
 	imageLoaded = False
 	proxy = None
+	pixMap = None
 
 	def __init__(self):
 		super(PogCanvas, self).__init__()
+		self.setMouseTracking(True)
+		# self.setAttribute(QtCore.Qt.WA_TransparentForMouseEvents)
 
 	def setPogData(self, pogData, fromRibbonBar):
 		self.fromRibbonBar = fromRibbonBar
@@ -26,14 +30,12 @@ class PogCanvas(QtWidgets.QLabel):
 	def setupWithPogData(self, pogData):
 		self.pogData = pogData
 		self.badURL = False
+		self.imageLoaded = False
 		if pogData.pogImageUrl != '':
 			self.setPogImageUrl(pogData.pogImageUrl)
-		else:
-			self.showImage = False
 
 	def setPogImageUrl(self, pogImageUrl):
 		self.currentPictureUrl = pogImageUrl
-		self.imageLoaded = False
 		self.pogData.pogImageUrl = pogImageUrl
 		imageUrl = ServicesManager.getDungeonManager().getUrlToDungeonResource(pogImageUrl)
 		AsyncImage(imageUrl, self.successfulLoaded, self.failedLoad).submit()
@@ -45,8 +47,9 @@ class PogCanvas(QtWidgets.QLabel):
 		:return: None
 		"""
 		image = asynchReturn.getData()
-		self.setPixmap(QtGui.QPixmap.fromImage(image))
-		self.setScaledContents(True)
+		self.pixMap = QtGui.QPixmap.fromImage(image)
+		self.imageLoaded = True
+		self.update()
 
 	def failedLoad(self, asynchReturn):
 		"""
@@ -72,7 +75,29 @@ class PogCanvas(QtWidgets.QLabel):
 
 	def setGridSize(self, size):
 		self.setGeometry(0, 0, int(size), int(size))
+		self.setFixedSize(int(size), int(size))
+		pass
 
 	def paintEvent(self, event):
-		super(PogCanvas, self).paintEvent(event)
+		# super(PogCanvas, self).paintEvent(event)
+		if not self.imageLoaded:
+			return
+		painter = QPainter(self)
+		pixmapSize = self.pixMap.size()
+		pixmapSize.scale(event.rect().size(), Qt.KeepAspectRatio)
+		pixmapScaled = self.pixMap.scaled(pixmapSize, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+		painter.drawPixmap(0, 0, pixmapScaled)
 		pass
+
+	def mouseMoveEvent(self, e):
+		"""
+		Move moved one me so start dragging
+		:param e: event
+		:return: None
+		"""
+		if e.buttons() == QtCore.Qt.LeftButton:
+			drag = QtGui.QDrag(self)
+			mime = QtCore.QMimeData()
+			drag.setMimeData(mime)
+			drag.setPixmap(self.grab())
+			drag.exec_(QtCore.Qt.MoveAction)
