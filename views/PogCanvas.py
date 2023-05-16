c1be58d4c3ac33190aa9ca7773b25acaf8771308
LLambert <lambertlb@aol.com>
2023-05-16 09:22:24 -0500

    finished drawing in pog canvas

--- views/PogCanvas.py
+++ views/PogCanvas.py
@@ -5,6 +5,8 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 from PyQt5.QtCore import Qt
 
 from services.AsyncTasks import AsyncImage
+from services.DungeonMasterFlag import DungeonMasterFlag
+from services.PlayerFlags import PlayerFlag
 from services.ReasonForAction import ReasonForAction
 from services.ServicesManager import ServicesManager
 from views.PopupMenu import PopupMenu
@@ -48,6 +50,8 @@ class PogCanvas(QtWidgets.QGraphicsItem):
 		self.pogData = pogData
 		self.badURL = False
 		self.imageLoaded = False
+		if pogData.pogNumber != 0:
+			self.setToolTip(str(pogData.pogNumber))
 		if pogData.pogImageUrl != '':
 			self.setPogImageUrl(pogData.pogImageUrl)
 
@@ -91,32 +95,66 @@ class PogCanvas(QtWidgets.QGraphicsItem):
 	def paint(self, painter: QtGui.QPainter, *args):
 		if not self.imageLoaded:
 			return
+		invisibleToPlayer = self.pogData.isDmFlagSet(DungeonMasterFlag.INVISIBLE_FROM_PLAYER)
+		isDM = ServicesManager.getDungeonManager().isDungeonMaster
+		if not isDM and invisibleToPlayer:
+			return
+		inEdit = ServicesManager.getDungeonManager().editMode
 		selected = ServicesManager.getDungeonManager().getSelectedPog()
 		isSelected = selected is not None and selected.isEqual(self.pogData)
-		border = self.computePogBorderWidth()
-		print(f'Border {border}')
 		scaledGridSize = self.getScaledGridSize()
-		if scaledGridSize != self.scaledGridSize or self.wasSelected != isSelected:
+		if scaledGridSize != self.scaledGridSize:
 			self.scaledGridSize = scaledGridSize
-			if selected:
-				self.scaledGridSize -= int(border * 2)
 			pixMap = QtGui.QPixmap.fromImage(self.image)
 			self.pixMap = pixMap.scaled(self.scaledGridSize, self.scaledGridSize, Qt.KeepAspectRatio,
 										Qt.SmoothTransformation)
-		if isSelected:
-			painter.setPen(QtGui.QPen(Qt.black, border, Qt.SolidLine))
+		darkInBackground = self.pogData.isDmFlagSet(DungeonMasterFlag.DARK_BACKGROUND)
+		transparent = self.pogData.isDmFlagSet(DungeonMasterFlag.TRANSPARENT_BACKGROUND)
+		if not inEdit and invisibleToPlayer:
+			painter.setOpacity(0.5)
 		else:
-			painter.setPen(QtGui.QPen(Qt.white, border, Qt.SolidLine))
-		painter.setBrush(QtGui.QBrush(Qt.white, Qt.SolidPattern))
-		painter.drawRect(0, 0, self.scaledGridSize, self.scaledGridSize)
-		painter.drawPixmap(border, border, self.pixMap)
+			painter.setOpacity(1.0)
+		if inEdit and darkInBackground:
+			painter.setBrush(QtGui.QBrush(Qt.black, Qt.SolidPattern))
+		else:
+			painter.setBrush(QtGui.QBrush(Qt.white, Qt.SolidPattern))
+		if not transparent:
+			painter.drawRect(0, 0, self.scaledGridSize, self.scaledGridSize)
+		elif inEdit and darkInBackground:
+			painter.drawRect(0, 0, self.scaledGridSize, self.scaledGridSize)
+
+		painter.drawPixmap(0, 0, self.pixMap)
+		self.drawOverlays(painter)
+		if isSelected:
+			border = self.computePogBorderWidth()
+			inset = int(border / 2)
+			length = self.scaledGridSize - inset
+			painter.setPen(QtGui.QPen(Qt.black, border, Qt.SolidLine))
+			painter.drawLine(inset, inset, length, inset)
+			painter.drawLine(inset, inset, inset, length)
+			painter.drawLine(length, length, 0, length)
+			painter.drawLine(length, length, length, 0)
+
 		self.wasSelected = isSelected
+
+	def drawOverlays(self, painter):
+		if self.pogData.isPlayerFlagSet(PlayerFlag.DEAD):
+			self.addDeadOverlay(painter)
+		pass
+
+	def addDeadOverlay(self, painter):
+		size = self.computePogBorderWidth()
+		painter.setPen(QtGui.QPen(Qt.red, size, Qt.SolidLine))
+		painter.setBrush(QtGui.QBrush())
+		painter.drawEllipse(0, 0, self.scaledGridSize, self.scaledGridSize)
+		painter.drawLine(0, 0, self.scaledGridSize, self.scaledGridSize)
+		pass
 
 	def computePogBorderWidth(self):
 		zoom = self.view.getZoom()
 		pogBorderWidth = zoom * self.gridSize / 10
-		if pogBorderWidth < 5.0:
-			pogBorderWidth = 5.0
+		if pogBorderWidth < 3.0:
+			pogBorderWidth = 3.0
 		if pogBorderWidth * 2 > self.getScaledGridSize():
 			pogBorderWidth = 0
 		return int(pogBorderWidth)
