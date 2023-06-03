e0e91b1153be0d6d28ba694ac87b4caa4e0381cb
LLambert <lambertlb@aol.com>
2023-06-03 09:36:16 -0500

    converted builder files using pyside2 instead of pyqt5

--- builder/convert.bat
+++ builder/convert.bat
@@ -1,6 +1,13 @@
-python -m PyQt5.uic.pyuic -x loginDialog.ui -o ../generated/loginDialog.py
-python -m PyQt5.uic.pyuic -x dungeonManagerDialog.ui -o ../generated/dungeonManagerDialog.py
-python -m PyQt5.uic.pyuic -x MainWindow.ui -o ../generated/MainWindow.py
-python -m PyQt5.uic.pyuic -x ViewPog.ui -o ../generated/ViewPog.py
-python -m PyQt5.uic.pyuic -x ViewNotes.ui -o ../generated/ViewNotes.py
-python -m PyQt5.uic.pyuic -x FlagSelector.ui -o ../generated/FlagSelector.py
+@REM python -m PyQt5.uic.pyuic -x loginDialog.ui -o ../generated/loginDialog.py
+@REM python -m PyQt5.uic.pyuic -x dungeonManagerDialog.ui -o ../generated/dungeonManagerDialog.py
+@REM python -m PyQt5.uic.pyuic -x MainWindow.ui -o ../generated/MainWindow.py
+@REM python -m PyQt5.uic.pyuic -x ViewPog.ui -o ../generated/ViewPog.py
+@REM python -m PyQt5.uic.pyuic -x ViewNotes.ui -o ../generated/ViewNotes.py
+@REM python -m PyQt5.uic.pyuic -x FlagSelector.ui -o ../generated/FlagSelector.py
+
+pyside2-uic loginDialog.ui -o ../generated/loginDialog.py
+pyside2-uic dungeonManagerDialog.ui -o ../generated/dungeonManagerDialog.py
+pyside2-uic MainWindow.ui -o ../generated/MainWindow.py
+pyside2-uic ViewPog.ui -o ../generated/ViewPog.py
+pyside2-uic ViewNotes.ui -o ../generated/ViewNotes.py
+pyside2-uic FlagSelector.ui -o ../generated/FlagSelector.py
